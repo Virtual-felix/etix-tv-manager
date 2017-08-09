1b2e8aa6ccea169d0d82e36b74b97e6b12405556
ffabrega <felix.fabrega@etixgroup.com>
2017-08-09 15:57:01 +0200

    ETXTV-15 Add folder system

--- admin-panel/src/constants/DesignApp.js
+++ admin-panel/src/constants/DesignApp.js
@@ -5,8 +5,7 @@ export default getMuiTheme({
     primary1Color: '#3391EE', // ETIX BLUE
     primary2Color: '#4AC948', // ETIX GREEN
     primary3Color: '#FF3333', // ETIX RED
+    primary4Color: '#C3C3C3', // ETIX LIGHT GREY
+    primary5Color: '#4F4F4F', // ETIX DARK GREY
   },
 });
-
-// ETIX LIGHT GREY: #C3C3C3
-// ETIX DARK GREY: #4F4F4F
